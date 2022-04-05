b9ca8dd9ab54aa13914d02db387f5aeecc565768
Hyperlynx <hjake123@gmail.com>
2022-04-04 20:00:29 -0500

    PieceAdd page

---
+++ Pages/PieceAdd.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dt_team2.Pages;
+
+public class PageAddModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public PageAddModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
