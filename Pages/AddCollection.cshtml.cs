74f7351c1df12998d7b8926ac63a9ae75c377baa
Dante A. Zorrilla <54299924+americozp@users.noreply.github.com>
2022-04-15 18:50:41 -0500

    Add collections to DB

--- Pages/AddCollection.cshtml.cs
+++ Pages/AddCollection.cshtml.cs
@@ -1,40 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace dt_team2.Pages;
-
 public class AddCollectionModel : PageModel
 {
     private readonly ILogger<AddCollectionModel> _logger;
-
+    private string connectionString = CSHolder.GetConnectionString();
     public AddCollectionModel(ILogger<AddCollectionModel> logger)
     {
         _logger = logger;
     }
+    public string CollectionName = default!;
+    public string Description = default!;
 
-    public void OnGet()
+    /*public ActionResult Collection_DE(Collections c)
     {
-    }
-
-
-    public string CollectionName { get; set; } = default!;
-    public string Description { get; set; } = default!;
-
-    // [HttpPost] 
-    public ActionResult Exhibition_DE(Collection c) {
-
-        string CollectionName = c.CollectionName;
-        string Description = c.Description;
+        CollectionName = c.CollectionName;
+        Description = c.Description;
 
         return null;
+    }*/
+
+    public void OnPost(Collections c)
+    {
+        CollectionName = c.CollectionName;
+        Description = c.Description;
+
+        //connect to database and insert query
+        if (CollectionName != "")
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand selectCommand = new SqlCommand("INSERT INTO [dbo].[Collections](CollectionName, Description) VALUES('" + CollectionName + "', '" + Description + "')", conn);
+                selectCommand.ExecuteNonQuery();
+
+                conn.Close();
+            }
+            Console.WriteLine("Collection Added");
+        }
+        else
+        {
+            //Console.WriteLine("Invalid Ticket Type ID:" );
+        }
+
     }
-
-    // submit form to OnPost
-    public void OnPost(Collection c) {
-
-        string ExhibitionName = c.CollectionName;
-        string Description = c.Description;
-    }
-
 
 }
--- Pages/Collections.cshtml.cs
+++ Pages/Collections.cshtml.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Data.SqlClient;
 
 namespace dt_team2.Pages;
+
+public class CollectionsOutput
+{
+    //Transactions Enitity Attributes
+    public string CollectionName { get; set; } = default!;
+    public string Description { get; set; } = default!;
+}
+
+public class Collections
+{
+    //Transactions Enitity Attributes
+    public string CollectionName { get; set; } = default!;
+    public string Description { get; set; } = default!;
+}
 
 public class CollectionsModel : PageModel
 {
     private readonly ILogger<CollectionsModel> _logger;
+    private string connectionString = CSHolder.GetConnectionString();
+    public static List<CollectionsOutput> col_output = new List<CollectionsOutput>();
 
     public CollectionsModel(ILogger<CollectionsModel> logger)
     {
@@ -14,7 +32,60 @@ public class CollectionsModel : PageModel
 
     public void OnGet()
     {
+        GetCollections();
     }
 
+    private void GetCollections()
+    {
+        //default query (nothing searched)
+        if (col_output.Count > 0)
+        {
+            col_output.Clear();
+        }
+        //Connect to database
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                //            Console.WriteLine("Database open");
+                SqlCommand selectCommand = new SqlCommand("SELECT * FROM [dbo].[Collections]", conn);
+                SqlDataReader results = selectCommand.ExecuteReader();
+
+                List<CollectionsOutput> temp_tr = new List<CollectionsOutput>();
+
+                //Get Main Transaction entity Info
+                while (results.Read())
+                {
+                    temp_tr.Add(new CollectionsOutput
+                    {
+                        CollectionName = results["CollectionName"].ToString(),
+                        Description = results["Description"].ToString(),
+                    });
+                }
+
+                //loop through transactions again to get all info needed
+                /*for (int i = 0; i < temp_tr.Count; i++)
+                {
+                    //Get Item Label
+                    int temp_colName = Convert.ToInt32(temp_tr[i].CollectionName);
+                    selectCommand = new SqlCommand("SELECT itemLabel FROM [dbo].[Lookup_Item] WHERE item = " + temp_colName, conn);
+                    results = selectCommand.ExecuteReader();
+                    while (results.Read())
+                    {
+                        temp_tr[i].CollectionName = results["CollectionName"].ToString();
+                    }
+                }*/
+
+                col_output = temp_tr;
+
+                conn.Close();
+            };
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
     public string Search { get; set; } = default!;
 }
