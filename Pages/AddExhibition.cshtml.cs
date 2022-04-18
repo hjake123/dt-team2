d619132a817668bd4c52b04f3e5217cf2223fd76
Andy Nguyencuu <62113803+andynguyencuu@users.noreply.github.com>
2022-04-18 02:12:27 -0500

    redir to dash upon post success

--- Pages/AddExhibition.cshtml.cs
+++ Pages/AddExhibition.cshtml.cs
@@ -28,32 +28,28 @@ public class AddExhibitionModel : PageModel
     string d = "', '";
 
     // submit form to OnPost
-    public void OnPost(Exhibition exhibition) {
-        ExhibitionName = exhibition.ExhibitionName;
-        Description = exhibition.Description;
-        // ListOfPieces = exhibition.ListOfPieces;
-        Arranger = exhibition.Arranger;
-        Location = exhibition.Location;
-        DateEnd = exhibition.DateEnd;
-        String[] fields = {ExhibitionName, Description, Arranger, Location};
-        if (DateEnd == new DateTime(1,1,1)) {
-            ModelState.AddModelError("DateEnd", "Please set a date for the exhibition to end!");
-            return;
-        }
-        if ((new String[] {ExhibitionName, Description, Arranger, Location}.All(field => !String.IsNullOrEmpty(field)))) {
-
-            using (SqlConnection connection = new SqlConnection(CSHolder.GetConnectionString())) {
-                connection.Open();
-                SqlCommand select = new SqlCommand("INSERT INTO dbo.Exhibitions(ExhibitionName, Description, Arranger, Location, DateEnd) VALUES('" + ExhibitionName + d + Description + d + Arranger + d + Location + d + DateEnd + "')", connection);
-                int rows_added = select.ExecuteNonQuery();
-                connection.Close();
-                Console.WriteLine(rows_added + "Exhibition added");
-            }
-            
-        } 
+    public IActionResult OnPost(Exhibition exhibition) {
+	    ExhibitionName = exhibition.ExhibitionName;
+	    Description = exhibition.Description;
+	    // ListOfPieces = exhibition.ListOfPieces;
+	    Arranger = exhibition.Arranger;
+	    Location = exhibition.Location;
+	    DateEnd = exhibition.DateEnd;
+	    String[] fields = {ExhibitionName, Description, Arranger, Location};
+	    if (DateEnd == new DateTime(1,1,1)) {
+		    ModelState.AddModelError("DateEnd", "Please set a date for the exhibition to end!");
+	    }
+	    else if ((new String[] {ExhibitionName, Description, Arranger, Location}.All(field => !String.IsNullOrEmpty(field)))) {
+		    using (SqlConnection connection = new SqlConnection(CSHolder.GetConnectionString())) {
+			    connection.Open();
+			    SqlCommand select = new SqlCommand("INSERT INTO dbo.Exhibitions(ExhibitionName, Description, Arranger, Location, DateEnd) VALUES('" + ExhibitionName + d + Description + d + Arranger + d + Location + d + DateEnd + "')", connection);
+			    int rows_added = select.ExecuteNonQuery();
+			    connection.Close();
+			    Console.WriteLine(rows_added + " Exhibition added");
+		    }
+		    return RedirectToPage("/Exhibitions");
+	    }
+	    return Page();
     }
-
-    
-
 
 }
