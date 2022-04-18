1189316fdabf3338335ac4f30db1f0aa833150f6
Andy Nguyencuu <62113803+andynguyencuu@users.noreply.github.com>
2022-04-18 02:10:05 -0500

    fix validate & post exhibition

--- Pages/Exhibition.cs
+++ Pages/Exhibition.cs
@@ -8,7 +8,7 @@ public class Exhibition {
 	public void OnGet() {}
     public string ExhibitionName { get; set; } = default!;
     public string Description { get; set; } = default!;
-    public string ListOfPieces { get; set; } = default!;
+    // public string ListOfPieces { get; set; } = default!;
     public string Arranger { get; set; } = default!;
     public string Location { get; set; } = default!;
     public DateTime DateEnd { get; set; } = default!;
