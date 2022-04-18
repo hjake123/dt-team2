7bc2da9cd75d56bd3f0358df32c795f8c86c4101
hyperlynx <hjake123@gmail.com>
2022-04-18 15:44:12 -0500

    hide sensitive info

--- ConnectionStringHolder.cs
+++
@@ -1,7 +0,0 @@
-namespace dt_team2;
-
-public class CSHolder{
-    public static string GetConnectionString(){
-        return "Server=tcp:dt-team2.database.windows.net,1433;Initial Catalog=dt-team2;Persist Security Info=False;User ID=team2admin;Password=COSC3380#;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-    }
-}
