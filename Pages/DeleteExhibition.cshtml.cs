a04235828e026c633d9dc2c08f7e30a1845fe1f5
Andy Nguyencuu <62113803+andynguyencuu@users.noreply.github.com>
2022-04-18 14:27:19 -0500

    flush out delete op

--- Pages/DeleteExhibition.cshtml.cs
+++ Pages/DeleteExhibition.cshtml.cs
@@ -15,49 +15,21 @@ public class DeleteExhibitionModel : PageModel {
 		ExhibitionName = id;
   }
 
-
 	public string ExhibitionName {
 		get; set;
 	} = default !;
-	public string Description {
-		get; set;
-	} = default !;
-	// public string ListOfPieces { get; set; } = default!;
-	public string Arranger {
-		get; set;
-	} = default !;
-	public string Location {
-		get; set;
-	} = default !;
-	public DateTime DateEnd {
-		get; set;
-	} = default !;
-
-	string d = "', '";
 
 	// submit form to OnPost
 	public IActionResult OnPost(Exhibition exhibition) {
 		ExhibitionName = exhibition.ExhibitionName;
-		Description = exhibition.Description;
-		// ListOfPieces = exhibition.ListOfPieces;
-		Arranger = exhibition.Arranger;
-		Location = exhibition.Location;
-		DateEnd = exhibition.DateEnd;
-		String[] fields = {ExhibitionName, Description, Arranger, Location};
-		if (DateEnd == new DateTime(1,1,1)) {
-			ModelState.AddModelError("DateEnd", "Please set a date for the exhibition to end!");
+		using (SqlConnection connection = new SqlConnection(CSHolder.GetConnectionString())) {
+			connection.Open();
+			SqlCommand select = new SqlCommand("DELETE FROM dbo.Exhibitions WHERE ExhibitionName='" + ExhibitionName + "'", connection);
+			int rows_deleted = select.ExecuteNonQuery();
+			connection.Close();
+			Console.WriteLine(rows_deleted + " Exhibition deleted");
 		}
-		else if ((new String[] {ExhibitionName, Description, Arranger, Location}.All(field => !String.IsNullOrEmpty(field)))) {
-			using (SqlConnection connection = new SqlConnection(CSHolder.GetConnectionString())) {
-				connection.Open();
-				SqlCommand select = new SqlCommand("INSERT INTO dbo.Exhibitions(ExhibitionName, Description, Arranger, Location, DateEnd) VALUES('" + ExhibitionName + d + Description + d + Arranger + d + Location + d + DateEnd + "')", connection);
-				int rows_added = select.ExecuteNonQuery();
-				connection.Close();
-				Console.WriteLine(rows_added + " Exhibition added");
-			}
 			return RedirectToPage("/Exhibitions");
-		}
-		return Page();
 	}
 
 }
