b9419a134a1f11bab679dfeca49f31718403b61a
Dante A. Zorrilla <54299924+americozp@users.noreply.github.com>
2022-04-19 10:52:46 -0500

    csholder added

---
+++ ConnectionStringHolder.cs
@@ -0,0 +1,9 @@
+﻿namespace dt_team2;
+
+public class CSHolder
+{
+    public static string GetConnectionString()
+    {
+        return "Server=tcp:dt-team2.database.windows.net,1433;Initial Catalog=dt-team2;Persist Security Info=False;User ID=team2admin;Password=COSC3380#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+}
