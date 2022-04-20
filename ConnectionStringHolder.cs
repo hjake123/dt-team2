1ec7a538bc87b2e9514fee9ad1f07b1162b5c8b0
Jalawaquin <57761181+Jalawaquin@users.noreply.github.com>
2022-04-20 18:39:55 -0500

    Set Multiple Active Sets to True
    Allows multiple queries in a single connection

--- ConnectionStringHolder.cs
+++ ConnectionStringHolder.cs
@@ -4,6 +4,6 @@ public class CSHolder
 {
     public static string GetConnectionString()
     {
-        return "Server=tcp:dt-team2.database.windows.net,1433;Initial Catalog=dt-team2;Persist Security Info=False;User ID=team2admin;Password=COSC3380#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        return "Server=tcp:dt-team2.database.windows.net,1433;Initial Catalog=dt-team2;Persist Security Info=False;User ID=team2admin;Password=COSC3380#;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
